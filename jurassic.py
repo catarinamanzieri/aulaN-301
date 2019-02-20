e8e3b5ccaaf977e084b6473a3be4cef70db6f4e2
Catarina <catarina.manzieri@gmail.com>
2019-02-19 22:06:03 -0300

    Aprender principios de Python

---
+++ jurassic.py
@@ -0,0 +1,74 @@
+
+# #
+
+# valor1 = input('digite o valor 1:')
+# valor2 = input('digite o valor 2:')
+# resultado = valor1 + valor2
+
+# if resultado >= 10 and resultado <=12:
+#     print('Está entre 10 e 12')
+
+# elif resultado == 13:
+#     print('opa opa eh 13')
+
+# else:
+#     print('não fizemos nenhum calc do seu valor')
+
+
+
+# Exercicio 1: faça um programa q receba dois valores inteiros e realize a soma, subtração, divisão, resto da divisão, média e multiplicação
+
+# valor1 = input('digite o valor 1:')
+# valor2 = input('digite o valor 2:')
+
+# resultado = int(valor1) + int(valor2)
+# print(f'seu resultado é {resultado}')
+
+
+
+# Exercicio 2: pedir nome e idade do usuario 
+# se o usuario for maior ou igual que 18 anos, aparecer em tela: "pode entrar nomedapessoa"
+
+# se o valor for menor que 11 anos, aparecer em tela: "Menor de idade, você ainda é um bebê"
+
+# caso contrário:
+# "Menor de idade"
+
+# valor1
+
+
+
+# Prox assunto : 
+
+# lista1 = ['cata manz', '22', 'Front End', True, 1.1]
+# print(f'{lista1[3]}')
+
+# rever esse assunto pra entender depois
+
+
+# import datetime
+
+# dia_de_nasc = datetime.date(day=11, year=1996, month=11)
+
+
+# def calcular_dias_de_vida(data):
+#     if data is None:
+#         error = 'ops'
+#         return error
+
+#     hoje = datetime.date.today()
+#     dias_de_vida = hoje - data
+#     return dias_de_vida
+
+# calcular_dias_de_vida(dia_de_nasc)
+
+
+
+
+
+
+
+
+
+
+
